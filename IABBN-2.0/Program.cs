df8e001f272774d36fd66f297c30555d8f3343a1
Ahsome <ahkamyaqeen@gmail.com>
2015-02-18 23:33:09 +1000

    Edited Program.CS to be more streamlined

--- IABBN-2.0/Program.cs
+++ IABBN-2.0/Program.cs
@@ -9,35 +9,48 @@ namespace Imabigboynow
 			Console.WriteLine ("Press '1' or '2'");
 			int Numberone = 0;
 			int Numbertwo = 0;
-			int End = 0;
+			bool End = false;
 			while(true)
 			{
-
 				int input = Console.ReadKey ().KeyChar;
-				if (input == '1' && End == 0) {
-					Numberone++;
-				} else if (input == '2' && End == 0) {
-					Numbertwo++;
+				if (!End)
+				{
+					if (input == '1') 
+					{
+						Numberone++;
+					} 
+					else if (input == '2') 
+					{
+						Numbertwo++;
+					}
 				}
 					
 				Console.Clear ();
-				Console.WriteLine (Numberone);
-				Console.WriteLine (Numbertwo);
+				Console.WriteLine ("{0}\n{1}",Numberone,Numbertwo);
 
-
-				if (Numberone>99 && Numbertwo<100){
+				if (Numberone>99 && Numbertwo<100)
+				{
 					Console.WriteLine ("1 wins");
-					End = 1;}
-				if (Numbertwo>99 && Numberone<100){
+					End = true;
+				}
+				else if (Numbertwo>99 && Numberone<100)
+				{
 					Console.WriteLine ("2 wins");
-					End = 1;}
-
-				if (Numberone>Numbertwo && Numberone<100){
-					Console.WriteLine ("1 is in the lead");}
-				if (Numbertwo>Numberone && Numbertwo<100){
-					Console.WriteLine("2 is in the lead");}
-				if (Numbertwo == Numberone){
-					Console.WriteLine("1 and 2 are currently tied");}
+					End = true;
+				}
+				else if (Numberone>Numbertwo && Numberone<100)
+				{
+					Console.WriteLine ("1 is in the lead");
+							}
+				else if (Numbertwo>Numberone && Numbertwo<100)
+				{
+					Console.WriteLine("2 is in the lead");
+				}
+				
+				if (Numbertwo == Numberone)
+				{
+					Console.WriteLine("1 and 2 are currently tied");
+				}
 			}
 		}
 	}
