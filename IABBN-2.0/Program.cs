e9f30f824b820abdb11811477c211aeb52f75c2f
somebody1234 <ehern081700@hotmail.com>
2015-03-26 16:41:59 +1000

    Refactored

--- IABBN-2.0/Program.cs
+++ IABBN-2.0/Program.cs
@@ -1,44 +1,53 @@
 ﻿using System;
+using System.Linq;
 
 class Iamabigboynow
 {
     public static void Main()
     {
         Console.WriteLine("Press '1' or '2'");
-        var one = 0;
-        var two = 0;
+        var score = new[] {0, 0};
+        var chars = new[] {'1', '2'};
         while (true)
         {
-            int input = Console.ReadKey().KeyChar;
-            switch (input)
-            {
-                case '1':
-                    one++;
-                    break;
-                case '2':
-                    two++;
-                    break;
-            }
-
+            var input = Console.ReadKey().KeyChar;
+            var index = Array.IndexOf(chars, input);
+            //TODO: improve find char
+            score[index]++;
             Console.Clear();
-            Console.WriteLine("{0}\n{1}", one, two);
+            //TODO: setposition
+            for (var i = 0; i < score.Length; i++) Console.WriteLine("{0}: {1}", chars[i], score[i]);
 
-            if (one > 99 && two < 100)
+            for (var i = 0; i < score.Length; i++)
             {
-                Console.WriteLine("1 wins");
+                if (!(score[i] > 99)) continue;
+                Console.WriteLine("{0} wins", i + 1);
                 Console.ReadKey();
                 return;
             }
-            if (two > 99 && one < 100)
+            var max = score.Max();
+            var maxScores = score.Where(t => t == max).ToArray();
+            var length = maxScores.Length;
+            var maxIndexes = new int[length];
+            var j = 0;
+            for (var i = 0; i < maxScores.Length; i++)
             {
-                Console.WriteLine("2 wins");
-                Console.ReadKey();
-                return;
+                if (maxScores[i] != max) continue;
+                maxIndexes[j] = i;
+                j++;
             }
-            if (one > two && one < 100) Console.WriteLine("1 is in the lead");
-            if (two > one && two < 100) Console.WriteLine("2 is in the lead");
-
-            if (two == one) Console.WriteLine("1 and 2 are currently tied");
+            if (maxScores.Length == 1) Console.WriteLine(chars[maxIndexes[0]] + " is in the lead");
+            else
+            {
+                var status = "";
+                for (var i = 0; i < length - 2; i++)
+                {
+                    status += chars[maxIndexes[i]] + ", ";
+                }
+                status += chars[maxIndexes[length - 2]] + " and " + chars[maxIndexes[length - 1]] + " are tied";
+                Console.WriteLine(status);
+            }
+            //TODO: add more stuff like more numbers/letters? more modes? set score to finish?
         }
     }
 }
